b9d3d143cc6a969ae9755f3034cb4087548d2158
satyamacn <satyam.arya@accenture.com>
2023-06-12 13:23:44 +0530

    struct-v2

--- struct/struct_test.go
+++ struct/struct_test.go
@@ -3,8 +3,14 @@ package main
 import "testing"
 import "fmt"
 
+type Rectangle struct{
+	width float64
+	length float64
+}
+
 func TestPerimeter(t *testing.T){
-	got := Perimeter(100.0,200.0)
+	rectangle := Rectangle{100.0,200.0}
+	got := Perimeter(rectangle)
 	want := 600.0
 
 	if got != want {
@@ -13,23 +19,23 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
-	got := Area(20.0,40.0)
-	want := 800.0
+	area := Rectangle{100,200}
+	got := Area(area)
+	want := 20000.0
 
 	if got != want {
 		t.Errorf("Got %.2f want %.2f",got,want)
 	}
 }
 
-func Area(length, width float64)float64 {
-	
-	area := length*width
+func Area(rectangle Rectangle)float64 {	
+	area := rectangle.length*rectangle.width
 	fmt.Println(area)
 	return area
 }
 
-func Perimeter(length, width float64)float64 {
-	perimeter := 2*(length+width)
+func Perimeter(rectangle Rectangle)float64 {
+	perimeter := 2*(rectangle.length+rectangle.width)
 	fmt.Println(perimeter)
 	return perimeter
 }
