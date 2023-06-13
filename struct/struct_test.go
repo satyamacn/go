ea4bbba4a35fd70769777cdfbb4459786d97057a
satyamacn <satyam.arya@accenture.com>
2023-06-13 11:57:21 +0530

    v

--- struct/struct_test.go
+++ struct/struct_test.go
@@ -1,7 +1,10 @@
 package main
 
-import "testing"
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"testing"
+)
 
 type Rectangle struct{
 	width float64
@@ -12,7 +15,12 @@ type Circle struct{
 	radius float64
 }
 
+type Shape interface {
+	Area() float64
+}
+
 func TestPerimeter(t *testing.T){
+	
 	rectangle := Rectangle{100.0,200.0}
 	got := Perimeter(rectangle)
 	want := 600.0
@@ -23,37 +31,37 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
+
+
+	checkArea := func(t testing.TB,shape Shape, want float64){
+		t.Helper()
+		got := shape.Area()
+		if got != want{
+			t.Errorf("got %.2f want %.2f",got,want)
+		}
+	}
+	
 	t.Run("rectangle",func(t *testing.T) {
 	rectangle := Rectangle{100,200}
-	got := Area(rectangle)
-	want := 20000.0
-
-	if got != want {
-		t.Errorf("Got %.2f want %.2f",got,want)
-	}
+	checkArea(t,rectangle,20000)
 	})
 
 	t.Run("circles",func(t *testing.T){
-		circle := Circle{12}
-		got := Area_c(circle)
-		want := 452.16
-
-		if got != want {
-			t.Errorf("Got %.2f want %f",got,want)
-		}	
+		circle := Circle{10}
+		checkArea(t,circle,314.1592653589793)	
 	})
 
 	
 }
 
-func Area(rectangle Rectangle)float64 {	
+func (rectangle Rectangle) Area() float64 {	
 	area := rectangle.length*rectangle.width
 	fmt.Println(area)
 	return area
 }
 
-func Area_c(circle Circle)float64{
-	area := 3.14*circle.radius*circle.radius
+func (circle Circle) Area() float64{
+	area := math.Pi*circle.radius*circle.radius
 	return area
 }
 
