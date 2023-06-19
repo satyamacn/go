8718dd74d17a044fe30126e6e1a604444796909c
satyamacn <satyam.arya@accenture.com>
2023-06-19 12:42:17 +0530

    v2

--- ds/fibonacci_test.go
+++ ds/fibonacci_test.go
@@ -1,12 +1,28 @@
-package main
+package ds
 
 import "testing"
 import "fmt"
 
+	func TestFibonacci(t *testing.T) {
+		testCases := []struct {
+			n    int
+			want string
+		}{
+			{2, "0 1"},
+			{5, "0 1 1 2 3"},
+			{10, "0 1 1 2 3 5 8 13 21 34"},
+		}
+	
+		for _, tc := range testCases {
+			t.Run(fmt.Sprintf("Fibonacci(%d)", tc.n), func(t *testing.T) {
+				got := fibonacci(tc.n)
+				fmt.Println(got)
+				if got != tc.want {
+					t.Errorf("Fibonacci(%d) = %q, want %q", tc.n, got, tc.want)
+				}
+			})
+		}
+		
+	}
 
-func Test( t *testing.T){
-	n := 10
-	fmt.Println("Fibonacci Series:")
-	fibonacci(n)
-
-}
+	
